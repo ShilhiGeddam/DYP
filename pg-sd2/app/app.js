ed0f6f55e03a85808f712701b49c47348ddadbf5
RevanthMudavath <rmudavath0817@gmail.com>
2024-12-17 02:17:25 +0000

    filters

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -1,83 +1,71 @@
-// Import express.js
+// Import express.js for routing and middleware
 const express = require("express");
 
-// Create express app
+// Initialize express application
 var app = express();
 
-// Add static files location
+// Serve static files (e.g., CSS, images, JS) from the 'static' folder
 app.use(express.static("static"));
 
-// Get the functions in the db.js file to use
+// Import database functions from db.js
 const db = require('./services/db');
-// Use the Pug templating engine
+
+// Set up Pug as the templating engine and define the views directory
 app.set('view engine', 'pug');
 app.set('views', './app/views');
-// Create a route for root - /
-// app.get("/", function(req, res) {
-//     res.render("navbar");
-// });
+
+// Route for login page
 app.get("/login", function(req, res) {
     res.render("login");
 });
+
+// Route for registration page
 app.get("/register", function(req, res) {
     res.render("register");
 });
+
+// Route for About Us page
 app.get("/about", function(req, res) {
     res.render("aboutus");
 });
-// Create a route for testing the db
+
+// Route for the homepage (testing database connection and fetching car data)
 app.get("/", function(req, res) {
     const brands = [
         "Toyota", "Honda", "Ford", "BMW", "Mercedes-Benz", "Audi",
         "Chevrolet", "Nissan", "Hyundai", "Volkswagen", "Tesla",
-        "Porsche", "Mazda", "Kia", "Subaru","Jaguar"
-      ];
-    // Assumes a table called test_table exists in your database
+        "Porsche", "Mazda", "Kia", "Subaru", "Jaguar"
+    ];
+    // Query the car_details table in the database
     sql = 'select * from car_details';
     db.query(sql).then(results => {
-        // console.log(results);
-        res.render('cars', { cars: results,brands });
+        // Render the 'cars' view with car data and available brands
+        res.render('cars', { cars: results, brands });
     });
 });
 
-// Route to display the details of a specific car
+// Route to display the details of a specific car based on carId
 app.get("/cars/:carId", function(req, res) {
     const { carId } = req.params;
     const sql = 'SELECT * FROM car_details WHERE car_id = ?';
-    console.log(sql);
     db.query(sql, [carId])
         .then(results => {
-            console.log(results.length);
+            // Check if the car exists and render its details
             if (results.length > 0) {
                 res.render('car_details', { car: results[0] });
             } else {
                 res.status(404).send('Car not found');
             }
-        })
+        });
 });
 
+// Route for contact page
 app.get("/contact", function(req, res) {
     res.render("contact");
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
-
-// Start server on port 3000
-app.listen(3000,function(){
+// Start the server on port 3000
+app.listen(3000, function() {
     console.log(`Server running at http://127.0.0.1:3000/`);
 });
