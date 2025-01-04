2847e3000f3831fa57b0a110fcf1c184e9dbb70c
SirishaUdatha <sirishaudatha2023@gmail.com>
2025-01-04 23:21:03 +0000

    login and register

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -1,8 +1,9 @@
 // Import express.js for routing and middleware
 const express = require("express");
-
+const { User } = require("./models/user");
 // Initialize express application
 var app = express();
+
 
 // Serve static files (e.g., CSS, images, JS) from the 'static' folder
 app.use(express.static("static"));
@@ -14,10 +15,25 @@ const db = require('./services/db');
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
-// Route for login page
-app.get("/login", function(req, res) {
-    res.render("login");
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
+app.use(cookieParser());
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const oneDay = 1000 * 60 * 60 * 24;
+const sessionMiddleware = session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
 });
+app.use(sessionMiddleware);
+
+// // Route for login page
+// app.get("/login", function(req, res) {
+//     res.render("login");
+// });
 
 // Route for registration page
 app.get("/register", function(req, res) {
@@ -29,8 +45,47 @@ app.get("/about", function(req, res) {
     res.render("aboutus");
 });
 
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send('Email and password are required.');
+        }
+
+        var user = new User(email);
+        const uId = await user.getIdFromEmail();
+        if (!uId) {
+            return res.status(401).send('Invalid email');
+        }
+
+        const match = await user.authenticate(password);
+        if (!match) {
+            return res.status(401).send('Invalid password');
+        }
+
+        req.session.uid = uId;
+        req.session.loggedIn = true;
+        console.log(req.session.id);
+        res.redirect('/cars');
+    } catch (err) {
+        console.error(`Error while authenticating user:`, err.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/logout', function (req, res) {
+    try {
+        req.session.destroy();
+        res.redirect('/login');
+    } catch (err) {
+        console.error("Error logging out:", err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 // Route for the homepage (testing database connection and fetching car data)
-app.get("/", function(req, res) {
+app.get("/cars", function(req, res) {
     const brands = [
         "Toyota", "Honda", "Ford", "BMW", "Mercedes-Benz", "Audi",
         "Chevrolet", "Nissan", "Hyundai", "Volkswagen", "Tesla",
@@ -43,6 +98,71 @@ app.get("/", function(req, res) {
         res.render('cars', { cars: results, brands });
     });
 });
+
+app.get("/login", function (req, res) {
+    try {
+        if (req.session.uid) {
+            res.redirect('/');
+        } else {
+            res.render('login');
+        }
+        res.end();
+    } catch (err) {
+        console.error("Error accessing root route:", err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/set-password', async function (req, res) {
+    const { email, password, contactNumber, address } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).send('Email and password are required.');
+    }
+
+    try {
+        const user = new User(email, contactNumber, address);
+
+        // Check if the user already exists
+        const uId = await user.getIdFromEmail();
+        if (uId) {
+            // If a valid, existing user is found, update the password
+            await user.setUserPassword(password);
+            console.log(`Password updated for user ID: ${uId}`);
+            return res.status(200).send('Password set successfully.');
+        } else {
+            // If no existing user is found, add a new one
+            await user.addUser(password,contactNumber,address);
+            console.log(`New user created with email: ${email}`);
+            return res.status(201).send('New user created successfully.');
+        }
+    } catch (err) {
+        console.error(`Error while setting password:`, err.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Route for contact page
+app.get("/contact", function(req, res) {
+    res.render("contact");
+});
+
+app.post('/contactus', async (req, res) => {
+    const { firstName, lastName, phoneNumber, email, message } = req.body;
+    const sql = 'INSERT INTO ContactUs (firstName, lastName, phoneNumber, email, message) VALUES (?, ?, ?, ?, ?)';
+
+    try {
+        db.query(sql, [firstName, lastName, phoneNumber, email, message], (err, result) => {
+            if (err) {
+                return res.redirect('contact', {status: 'error', message: 'Failed to insert into ContactUs table' });
+            }
+            res.render('contact',{ status: 'success', message: 'Contact details inserted successfully' });
+        });
+    } catch (err) {
+        res.render('contact',{ status: 'error', message: 'Failed to insert into ContactUs table' });
+    }
+});
+
 
 // Route to display the details of a specific car based on carId
 app.get("/cars/:carId", function(req, res) {
@@ -59,10 +179,7 @@ app.get("/cars/:carId", function(req, res) {
         });
 });
 
-// Route for contact page
-app.get("/contact", function(req, res) {
-    res.render("contact");
-});
+
 
 
 // Start the server on port 3000
